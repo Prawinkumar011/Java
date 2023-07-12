7cf52ee50fe8c99a46459ab7fba422eff7d44be3
Prawinkumar011 <114295435+Prawinkumar011@users.noreply.github.com>
2023-07-12 11:58:00 +0530

    Update All___Number.java

--- DAY 2/All___Number.java
+++ DAY 2/All___Number.java
@@ -1,6 +1,5 @@
 class All___Number{
     public static void main(String[] args) {
-        // to print first hundred numbers
         for( int i=100;i>=0;i=i-3){
         System.out.print( i+ "  ");
         }
